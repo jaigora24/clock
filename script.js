95398228316927943c207ca60d182dc4eb19c3a6
Jacek <jacenty.kubas@gmail.com>
2018-05-22 22:13:51 +0200

    js css clock

---
+++ script.js
@@ -0,0 +1,22 @@
+const handSeconds = document.querySelector('.clock__seconds');
+const handMinutes = document.querySelector('.clock__minutes');
+const handHours = document.querySelector('.clock__hour');
+
+function setDate () {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    
+    handSeconds.style.transform = 'rotate('+ secondsDegrees +'deg)';
+    handMinutes.style.transform = 'rotate('+ minutesDegrees +'deg)';
+    handHours.style.transform = 'rotate('+ hoursDegrees +'deg)';
+    console.log(minutes);
+}
+
+setInterval(setDate, 1000);
